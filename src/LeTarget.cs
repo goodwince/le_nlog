3544ed3c85dc58fd94d35fe3cac2b4a2167533b2
Mark Lacomber <marklacomber@gmail.com>
2012-06-21 13:56:33 +0200

    Changed string to constant

--- src/LeTarget.cs
+++ src/LeTarget.cs
@@ -67,6 +67,8 @@ namespace Le
         public static readonly int QUEUE_SIZE = 32768;
         /** Logentries API server address. */
         static readonly String LE_API = "api.logentries.com";
+        /** Logentries Client Authentication */
+        static readonly String LE_AUTH_CLIENT = "logentries.com";
         /** Default port number for Logentries API server. */
         static readonly int LE_PORT = 80;
         /** Default SSL port number for Logentries API server. */
@@ -350,7 +352,7 @@ namespace Le
                 if (Ssl)
                 {
                     this.stream_ssl = new SslStream(this.stream);
-                    this.stream_ssl.AuthenticateAsClient("logentries.com");
+                    this.stream_ssl.AuthenticateAsClient(LE_AUTH_CLIENT);
                 }
             }
 
